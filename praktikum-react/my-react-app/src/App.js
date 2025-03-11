5374ee6111fb729c49bb42159201e4d62f0560a9
DeaPutri17 <putrinastitidea@gmail.com>
2025-03-11 18:28:11 +0700

    Menggunakan State untuk Mengelola Data

--- praktikum-react/my-react-app/src/App.js
+++ praktikum-react/my-react-app/src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+//import React from 'react';
 import Counter from './Counter';
 import Greeting from './Greeting';
+import React, {useState} from "react";
 
+function Example() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [email, setEmail] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  return(
+    <div>
+      <input type="text" placeholder="Nama" value={name} onChange={handleNameChange} />
+      <input type="number" placeholder="Umur" value={age} onChange={handleAgeChange} />
+      <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+      <p>{name} berumur {age} tahun dan emailnya adalah {email}.</p>
+    </div>
+  );
+}
 
 //komponen header
 function Header(){
@@ -39,6 +66,7 @@ function App(){
       <Main />
       <Greeting name="Dea" />
       <Counter />
+      <Example />
       <Footer />
     </div>
   );
